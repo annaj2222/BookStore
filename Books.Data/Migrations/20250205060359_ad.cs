18e381f7875158fd3bc3548e9ba18478deab0690
annaj222 <andrianajaneva2002@gmail.com>
2025-02-05 19:08:22 +0100

    Add project files.

---
+++ Books.Data/Migrations/20250205060359_ad.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Books.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ad : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "ShoppingCarts",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "ShoppingCarts",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Books.Domain/Utilities/FacebookSettings.cs
@@ -0,0 +1,9 @@
+﻿//namespace Books.Domain.Utilities
+//{
+//    public class
+//    Settings
+//    {
+//        public string AppId { get; set; } = string.Empty;
+//        public string AppSecret { get; set; } = string.Empty;
+//    }
+//}
